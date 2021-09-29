09fd604c3ae7b6fc3e05a81f159c8c5945f1da22
Rohit Jadhav <Rohit.Jadhav@hansencx.com>
2021-09-29 16:50:43 +0530

    rohit

---
+++ src/main/java/com/hansen/mobileplan/model/Auditlog.java
@@ -0,0 +1,55 @@
+package com.hansen.mobileplan.model;
+
+import java.util.Date;
+
+//It's just a POJO not entity class
+public class Auditlog {
+	
+	private Long id;
+	private String operationType;
+	private String entityJson;
+	private Date modificationDate;
+	
+	public Auditlog() {
+		super();
+	}
+
+	public Auditlog(String operationType, String entityJson) {
+		super();
+		this.operationType = operationType;
+		this.entityJson = entityJson;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOperationType() {
+		return operationType;
+	}
+
+	public void setOperationType(String operationType) {
+		this.operationType = operationType;
+	}
+
+	public String getEntityJson() {
+		return entityJson;
+	}
+
+	public void setEntityJson(String entityJson) {
+		this.entityJson = entityJson;
+	}
+
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(Date modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+
+}
